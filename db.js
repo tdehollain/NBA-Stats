f5b1d0443a9da6f657f04f0fe3fc712ad04498f1
Thibaut de Hollain <tdehollain@Thibauts-MBP.lan>
2017-01-07 23:10:54 +0100

    new port for mongodb

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // MongoDB Connection & Schema
-mongoose.connect('mongodb://localhost:27017/nba', (err) => {
+mongoose.connect('mongodb://localhost:9001/nba', (err) => {
 	if(err) {
 		console.log('Error connecting to MongoDB: ' + err);
 	} else {
